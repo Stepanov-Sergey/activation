ed606ac983a9f988e2f8f8faf4f4719089061f45
Sergey Stepanov <sergey.stepanov@lenta.ru>
2023-07-30 23:02:44 +0300

    Swish.py
    +to do

--- Swish.py
+++ Swish.py
@@ -39,7 +39,39 @@ class Swish:
     def relu(x):
         """Swish activation function using relu."""
         return x * Swish.activation_relu(x)
-        
+
+''''
+The `swish` activation function can be combined with various other activation functions. Some common choices include:
+
+- ReLU (Rectified Linear Unit): return x * max(0, x)
+- Leaky ReLU: return x * max(0.01 * x, x)
+- Tanh (Hyperbolic tangent):return x * np.tanh(x)
+- ELU (Exponential Linear Unit):return x if x >= 0 else np.exp(x) - 1
+- Parametric ReLU (PReLU): return x * max(alpha * x, x) #(where `alpha` is a learnable parameter)
+- Exponential Linear Unit (ELU): return x if x >= 0 else alpha * (np.exp(x) - 1)
+- Softplus: return x * np.log(1 + np.exp(x))
+- Gaussian Error Linear Unit (GELU):return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
+- Swish-1:return x * sigmoid(beta * x)
+- Mish: return x * np.tanh(np.log(1 + np.exp(x)))
+- Softsign: `return x / (1 + np.abs(x))`
+- ArcTan: `return np.arctan(x)`
+- Bent Identity: `return (np.sqrt(x*x + 1) - 1) / 2 + x`- Cube: `return np.power(x, 3)`
+- Sinusoid: `return np.sin(x)`
+- Inverse Square Root Unit (ISRU): `return x / np.sqrt(1 + alpha * np.power(x, 2))` (where `alpha` is a hyperparameter)
+- SoftExponential: `return np.where(x < 0, -np.log(1 - alpha * (x + alpha)) / alpha, x)`
+- SineReLU: `return np.where(x > 0, np.sin(x), alpha * np.exp(x) - alpha)`
+- Gaussian: `return np.exp(-np.power(x, 2))`
+- ISRLU (Inverse Square Root Linear Unit): `return np.where(x > 0, x, x / np.sqrt(1 + alpha * np.power(x, 2)))` (where `alpha` is a hyperparameter)
+- Bent Identity: `return (np.sqrt(x * x + 1) - 1) / 2 + x`
+- LogLog: `return np.log(np.log(x + 1) + 1)`
+- SoftClip: `return np.clip(x, -alpha, alpha)`- HardSwish: `return x * np.clip(x + 3, 0, 6) / 6`
+- Symmetric Sigmoid: `return 2 / (1 + np.exp(-x)) - 1`
+- SoftExponential: `return np.where(x < 0, -np.log(1 - alpha * (x + alpha)) / alpha, x)`
+- Gaussian Error Linear Unit (GELU): `return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))`
+    
+
+These are just a few examples. You can combine the `swish` function with any other activation function that suits your needs.
+'''
 '''
 ### !!!!!!!!!!!!
 ### Обратите внимание, что в функции swish мы используем сигмоидную функцию активации, но Swish может работать с другими функциями активации, такими как ReLU. Вы можете использовать другие функции активации, если необходимо. Кроме того, для повышения производительности такие функции могут быть реализованы с использованием библиотек для машинного обучения, таких как TensorFlow или PyTorch.
