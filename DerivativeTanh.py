a4ed0c0734e2a1151e41c5542f7782bc7266af91
Sergey Stepanov <sergey.stepanov@lenta.ru>
2023-07-30 22:13:03 +0300

    DerivativeTanh.py

--- DerivativeTanh.py
+++ DerivativeTanh.py
@@ -1,6 +1,7 @@
 '''
+Hyperbolic tangent derivative (tanh)
 Производная гиперболического тангенса (tanh) на языке Python может быть вычислена с использованием библиотеки sympy.от пример кода:
-
+'''
 python
 import sympy as sp
 
@@ -12,7 +13,10 @@ derivative = sp.diff(sp.tanh(x), x)
 
 # Вывод результата
 print(derivative)
-Этот код использует библиотеку sympy для символьных вычислений. Он определяет символьную переменную x и затем вычисляет производную гиперболического тангенса tanh(x) по переменной x. Результат выводится на экран.
 
+'''
+Этот код использует библиотеку sympy для символьных вычислений. Он определяет символьную переменную x и затем вычисляет производную гиперболического тангенса tanh(x) по переменной x. Результат выводится на экран.
 Выходные данные будут представлены в виде символьного выражения, которое представляет производную гиперболического тангенса.
+This code uses the sympy library for symbolic calculations. It defines a symbolic variable x and then calculates the derivative of the hyperbolic tangent tanh(x) over the variable x. The result is displayed on the screen.
+The output data will be represented as a symbolic expression that represents the derivative of the hyperbolic tangent.
 '''
